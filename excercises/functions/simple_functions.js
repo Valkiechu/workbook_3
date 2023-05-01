54e0ad6b6394a3f3b95b372397ff75ba2c1e4eb4
Ethan Qiu <qethan1996@gmail.com>
2023-05-01 11:35:38 -0400

    demos

---
+++ excercises/functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings(){
+    console.log("My Favorite Things are: " );
+    console.log(" My favorite music is Kpop");
+    console.log(" My favorite movie is John Wick");
+    console.log(" My favorite game is idk   ");
+
+
+}
+
+favoriteThings();
+
+function whyImHere(){
+
+   console.log(" i want to go to bed");
+}
+
+whyImHere();
